a3f5bad8ad9b5a5801d097d982276ac1d854d621
shiref mohammed <shirefmo012345@gmail.com>
2023-09-14 13:46:04 +0200

    update files

--- server/src/server.js
+++ server/src/server.js
@@ -2,11 +2,10 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const _PORT = process.env.PORT;
-
 const authenticationRouter = require("./routes/authenticationRouter");
 const usersRouter = require("./routes/usersRouter");
 const recipesRouter = require("./routes/recipesRouter");
+const _PORT = process.env.PORT;
 
 const app = express();
 mongoose.connect(process.env.MONGO_CONNECTION);
@@ -18,5 +17,6 @@ app.use(express.json());
 app.use("/authentication", authenticationRouter);
 app.use("/users", usersRouter);
 app.use("/recipes", recipesRouter);
+app.get("/", async (req, res) => res.send("home page"));
 
 app.listen(_PORT, () => console.log(`SERVER WORK GOOD ON PORT ${_PORT}`));
