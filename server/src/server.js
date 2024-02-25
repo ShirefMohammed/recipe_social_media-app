f0107358166c6be34a529a946ea4833fbc9f06ce
shiref mohammed <shirefmo012345@gmail.com>
2024-02-25 12:48:32 +0200

    Adding ping server internal for render

--- server/src/server.js
+++ server/src/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const http = require("http");
 const authenticationRouter = require("./routes/authenticationRouter");
 const usersRouter = require("./routes/usersRouter");
 const recipesRouter = require("./routes/recipesRouter");
@@ -9,6 +10,7 @@ const _PORT = process.env.PORT;
 
 const app = express();
 mongoose.connect(process.env.MONGO_CONNECTION);
+console.log(process.env.MONGO_CONNECTION)
 
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
@@ -20,3 +22,8 @@ app.use("/recipes", recipesRouter);
 app.get("/", async (req, res) => res.send("home page"));
 
 app.listen(_PORT, () => console.log(`SERVER WORK GOOD ON PORT ${_PORT}`));
+
+// Function to ping the server every 10 minutes
+setInterval(() => {
+  http.get(process.env.SERVER_URL);
+}, 600000);
